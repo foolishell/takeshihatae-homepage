96f3cc944e98bd1c4256945b12c1a4e34c69b3c4
Kai Fukushima <k_fukushima@yumemi.co.jp>
2021-09-11 18:05:28 +0900

    feat: add facebook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import "./Home.scss"
 import indexLg from "../assets/index--lg.jpg"
 import indexMd from "../assets/index--md.jpg"
+import facebook from "../assets/facebook.png"
 import { Links } from "../components/Links"
 
 const Menu = () => {
@@ -9,6 +10,9 @@ const Menu = () => {
     <div>
       <div className="index__menu">
         <Links />
+        <a className="menu__link" href="https://m.facebook.com/people/Takeshi-Hatae/100007454224969">
+          <img alt="" className="index__facebook" src={facebook}  />
+        </a>
       </div>
     </div>
   )
